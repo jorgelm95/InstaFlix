760984df8117402b66783e514511b618a1e12efc
Jorge luis Menco <jmenco@condorlabs.io>
2024-10-07 22:59:31 -0500

    fix crash when data not found

--- InstaFlixData/Sources/InstaFlixData/Movies/Service/Request/FetchMoviesRequest.swift
+++ InstaFlixData/Sources/InstaFlixData/Movies/Service/Request/FetchMoviesRequest.swift
@@ -13,18 +13,21 @@ struct FetchMoviesRequest: RequestType {
     typealias Output = ResponseResult<[APIMovie]>
     
     var baseUrl: URL { URL(string: "https://run.mocky.io/")! }
-    var path: String { "v3/237fc551-d059-4aee-993a-064684842627" }
+    var path: String { "v3/5b0e6097-33c3-4ef4-b4cc-26bc8d1484f1" }
     var queryParams: HTTPQueryParams {[:]}
     var method: HTTPMethod { .get }
     
     func decode(data: Data, httpCode: Int) throws -> ResponseResult<[APIMovie]> {
         let decoder = JSONDecoder()
 
-        printData(for: data)
-        
-        return Output(
-            response: try decoder.decode(Output.Response.self, from: data),
-            httpCode: httpCode)
+        if  !data.isEmpty {
+            printData(for: data)
+            return Output(
+                response: try decoder.decode(Output.Response.self, from: data),
+                httpCode: httpCode)
+        } else {
+            return  ResponseResult<[APIMovie]>(response: [], httpCode: httpCode)
+        }
     }
     
     private func printData(for data: Data) {
@@ -42,9 +45,6 @@ struct FetchMoviesRequest: RequestType {
             options: [.prettyPrinted, .sortedKeys]
         )
         let prettyPrintedString = String(data: prettyPrintedData!, encoding: .utf8)!
-        print(prettyPrintedString)
-        
-//        return object as? [String: Any]
         
         return try? JSONSerialization.jsonObject(with: prettyPrintedData!, options: []) as? [String: Any]
     }
